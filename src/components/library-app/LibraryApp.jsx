8bb0cc032fd8d47fa84a6f20c8113126388921c6
lianne24 <124648058+lianne24@users.noreply.github.com>
2025-10-27 21:12:32 -0400

    Added Login and Welcome Components

--- src/components/library-app/LibraryApp.jsx
+++ src/components/library-app/LibraryApp.jsx
@@ -2,6 +2,25 @@ export default function LibraryApp(){
     return (
         <div className="LibraryApp">
             Library App
+            <Login/>
+            <Welcome/>
+        </div>
+        
+    )
+}
+
+function Login(){
+    return (
+        <div className="Login">
+            Login Component
+        </div>
+    )
+}
+
+function Welcome(){
+    return (
+        <div className="Welcome">
+            Welcome Component
         </div>
     )
 }
